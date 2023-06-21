c3402933207c279e98ef70782d7eaf6352b81371
Shobi172 <shobinshaju2001@gmail.com>
2023-06-21 09:22:03 +0530

    port modified

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(router);
 
-const PORT = process.env.PORT || 5000; 
+const PORT = 5000; 
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
